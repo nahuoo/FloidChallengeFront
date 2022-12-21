bfa376a832d399cc565db0f0fd14af2b0323fdc1
nahuel@chipax.com <nahuel@chipax.com>
2022-12-21 11:41:29 -0300

    feat: agrega react router

--- src/index.js
+++ src/index.js
@@ -1,14 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { Santander } from './components/santander';
 import reportWebVitals from './reportWebVitals';
 import { GlobalStyle } from './globalStyle'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/santander",
+    element: <Santander />,
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <GlobalStyle />
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 reportWebVitals();
