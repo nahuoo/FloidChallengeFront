22648e9407d0bb0ab9c3fe1aedaf4eff3e32d6c0
nahuel@chipax.com <nahuel@chipax.com>
2022-12-21 11:42:02 -0300

    feat: view de inputs

--- src/components/home.js
+++ src/components/home.js
@@ -1,71 +1,15 @@
-import { useState } from "react"
-import styled from "styled-components"
-import axios from "axios"
+import { Container, Gray, Button } from "../styles"
 
-export const Home = () =>  {
-    const [santanderAccounts, setSantanderAccounts ] = useState('')
+export const Home = () => {
 
-    const handleclick = async () => {
-       const response = await axios.post('https://floidback.onrender.com/get',
-        {
-            id: "11111111-1",
-            password: 1234,
-        })
-
-        setSantanderAccounts(response[0]);
-    }
-  
-    if (santanderAccounts) return (
-        <Container>{santanderAccounts.map(account => (
-            <>
-            <div>{account.type}</div>
-            <div>{account.number}</div>
-            <div>{account.currency}</div>
-            <div>{account.balance}</div>
-            </>
-            ))}
-        </Container>
-    )
-     return (
-        <Container>
-            <img src="https://admin.floid.app/widget/assets/logos_clientes/Floid-Logo-circulo.png" height={80} alt="" />
-            <h1>Comparte tu información bancaria con <b>Floid</b> usando tus credenciales.</h1>
-            <Gray>La información viaja encriptada de extremo a extremo.</Gray>
-            <Gray>Tus datos te pertenecen y serán compartidos sólo con Floid.</Gray>
-            <Button onClick={handleclick} >Comenzar</Button>
-            <img src="https://admin.floid.app/widget/assets/img/Logo_floid.svg" alt="" />
-        </Container>
-    )
+  return (
+    <Container>
+      <img src="https://admin.floid.app/widget/assets/logos_clientes/Floid-Logo-circulo.png" height={80} alt="" />
+      <h1>Comparte tu información bancaria con <b>Floid</b> usando tus credenciales.</h1>
+      <Gray>La información viaja encriptada de extremo a extremo.</Gray>
+      <Gray>Tus datos te pertenecen y serán compartidos sólo con Floid.</Gray>
+      <Button to="/santander">Comenzar con Santander</Button>
+      <img src="https://admin.floid.app/widget/assets/img/Logo_floid.svg" alt="" />
+    </Container>
+  )
 }
-
-const Container = styled.div `
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: space-around;
-    background: white;
-    padding: 1rem;
-    border: #f1f1f1 1px solid ;
-    height: 90vh;
-    width: 30vw;
-    font-size: 1.2rem;
-
-`
-
-const Gray = styled.div `
-   color: gray;
-   font-size: 0.8rem;
-`
-
-const Button = styled.button`
-   background: #182750;
-   padding: 1.4rem;
-   border-radius: 2%;
-   color: white;
-   width: 90%;
-   cursor: pointer;
-   transition: 0.3s;
-   :hover{
-    background: #2b4a9e;
-   }
-`
---
+++ src/components/santander.js
@@ -0,0 +1,80 @@
+import { Link } from "react-router-dom";
+import { useState } from "react";
+import axios from "axios";
+import { Wrapper, WidgetContainer, Container, Card, Button, InputContainer, Label, Input } from "../styles"
+import { checkRut } from "../utils/checkRut";
+
+export const Santander = () => {
+    const [santanderAccounts, setSantanderAccounts] = useState('')
+    const [rutInput, setRutInput] = useState('')
+    const [validRut, setValidRut] = useState('')
+    const [password, setPassword] = useState('')
+    const [isLoading, setIsLoading] = useState(false)
+
+    const handleRutInput = (e) => {
+        setValidRut(checkRut(e.target.value))
+        setRutInput(e.target.value)
+        console.log(validRut)
+    }
+    const handleclick = async () => {
+
+        try {
+            setIsLoading(true)
+            const response = await axios.post('https://floidback.onrender.com/get',
+                {
+                    id: "11111111-1",
+                    password: 1234,
+                },
+            )
+            setSantanderAccounts(response.data);
+            console.log(response.data)
+            setIsLoading(false)
+        } catch (error) {
+            setSantanderAccounts(error);
+            console.log(error)
+        }
+
+    }
+
+    if (santanderAccounts) return (
+
+        <Container santanderAccounts>
+            {santanderAccounts.map((account, index) => {
+                return (
+                    
+                      
+                       
+                            <Card key={index}>
+                                <p>Tipo de cuenta: {account.type}</p>
+                                <p>Numero de cuenta: {account.number}</p>
+                                <p>Tipo de Moneda: {account.currency}</p>
+                                <p>Balance: {account.balance}</p>
+                            </Card>
+                           
+                       
+                    
+                )
+            })}
+        </Container>
+    )
+    return (
+        <Wrapper isLoading={isLoading}>
+            <WidgetContainer>
+                <Container>
+                    <img src="https://admin.floid.app/widget/assets/logos/Santander.svg" height={40} alt="" />
+                    <InputContainer>
+                        <Label rutInput>Ingresa tu RUT</Label>
+                        <Input name="rut" type="text" required onChange={handleRutInput} />
+                        <p>{validRut}</p>
+                    </InputContainer>
+                    <InputContainer>
+                        <Label password>Contraseña</Label>
+                        <Input name="password" type="password" required onChange={e => setPassword(e.target.value)} />
+                    </InputContainer>
+                    <Button onClick={handleclick} >{isLoading ? '...' : 'Continuar'}</Button>
+                    <img src="https://admin.floid.app/widget/assets/img/Logo_floid.svg" alt="" />
+                </Container>
+            </WidgetContainer>
+        </Wrapper>
+    )
+}
